244c8b6637590c420f656893b452ebfb58b84675
Anand <anand.a.anand@capgemini.com>
2020-05-01 08:26:27 +1000

    adding initial project to remote repository

---
+++ ios/Pods/Target Support Files/Pods-SGRGBSS-SGRGBSSTests/Pods-SGRGBSS-SGRGBSSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SGRGBSS_SGRGBSSTests : NSObject
+@end
+@implementation PodsDummy_Pods_SGRGBSS_SGRGBSSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-SGRGBSS/Pods-SGRGBSS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SGRGBSS : NSObject
+@end
+@implementation PodsDummy_Pods_SGRGBSS
+@end
